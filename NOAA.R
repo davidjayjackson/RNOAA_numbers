d91a2311a70a1a62b65df5252006c8df6516d48c
davidjayjackson <davidjayjackson@gmail.com>
2019-02-27 18:15:19 -0500

    Created summary data.frames: Day,Month,Year And basic plots.

--- NOAA.R
+++ NOAA.R
@@ -3,6 +3,8 @@ library(forecast)
 library(lubridate)
 library(xts)
 library(ggplot2)
+library(sqldf)
+library(RSQLite)
 
 daily <-fread("https://www.aavso.org/sites/default/files/solar/NOAAfiles/NOAAdaily.csv")
 monthly <- fread("https://www.aavso.org/sites/default/files/solar/NOAAfiles/NOAAmonthly.csv")
@@ -27,3 +29,21 @@ plot(daily$Ymd,daily$Ra)
 lines(daily$Ymd,daily$Ra,col="red")
 ggplot(data=monthly,aes(Year,Ra)) + geom_smooth() +geom_point()
 ggplot(data=daily,aes(Ymd,Ra)) + geom_point() +geom_smooth()
+#
+# sqldf stuff and plots
+# Yearly summary
+mty <- sqldf("Select Year,avg(Ra) as ct from monthly where Ra >=1 group by Year")
+ggplot(mty,aes(Year,ct)) +geom_bar(stat="summary",fun.y="sum")  + geom_smooth() +ggtitle("Total Sunspots by Year")
+#
+# Monthly Summary
+mtm <- sqldf("Select Month,avg(Ra) as ct from monthly where Ra >=1 group by Month")
+ggplot(data=mtm,aes(Month,ct)) + geom_smooth() +geom_line()
+# Day Summary
+mtd <- sqldf("Select Day,avg(Ra) as ct from daily where Ra >=1 group by Day")
+ggplot(data=mtd,aes(Day,ct,size=ct)) + geom_smooth() +geom_point() + ggtitle("Sunspot Summary by Day")
+# Bar Plots + Smooth
+#
+
+
+
+
